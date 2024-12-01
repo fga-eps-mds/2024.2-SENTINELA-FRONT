70b3eab3885bee2137763aa72438069e950c22ae
Jorge Henrique <jorgehenriquelessaoliveira@gmail.com>
2024-12-01 19:43:00 -0300

    Criação do modal das vantagens Co-authored-by: Rodrigo Amaral rodrigoamaral1705@gmail.com Co-authored-by: Joao Igor joaoigorpereira@gmail.com

---
+++ src/Components/AdvantagesModal/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./index.css";
+import { FaBalanceScale } from "react-icons/fa";
+
+export default function AdvantagesModal() {
+  return (
+    <>
+      <div className="modal-overlay">
+        <div className="modal-box">
+          <button className="close-button">
+            &times;
+          </button>
+          <div className="modal-header">
+            <FaBalanceScale size={40} className="modal-icon" />
+            <h2>Titulo do Beneficio</h2>
+          </div>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+          <p>
+            Para mais informações, entre em contato com o Sindicato pelo número{" "}
+            <b>(61) 3321-1949</b>.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
