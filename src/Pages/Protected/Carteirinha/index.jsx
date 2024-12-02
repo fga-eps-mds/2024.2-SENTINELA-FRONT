929b1d589fa68b3550b5e5220937512d3a20b49b
daniso0412 <180015222@aluno.unb.br>
2024-12-02 12:24:24 -0300

    Feature:US1 - Função de downloardpdf criado Co-authored-by: Mariana <martins.mariana2013@gmail.com> Co-authored-by: Igor <igor.veras.daniel@gmail.com> Co-authored-by: Dannyeclisson  <dannyeclisson@gmail.com>

--- src/Pages/Protected/Carteirinha/index.jsx
+++ src/Pages/Protected/Carteirinha/index.jsx
@@ -1,56 +1,127 @@
-import { useAuth } from "../../../Context/auth";
-import { useNavigate } from "react-router-dom";
-import SecondaryButton from "../../../Components/SecondaryButton";
+import React, { useRef }from "react";
 import "./index.css";
-import sindpol_logo from "../../../assets/sindpol-logo.png";
-import sentinela_logo from "../../../assets/sentinela-logo.png";
-import { checkAction, usePermissions } from "../../../Utils/permission";
-import LabeledTextField from "../../../../src/Components/LabeledTextField";
-import DataSelect from "../../../Components/DataSelect/index.jsx";
-import LogoSentinela from "../../../assets/sindpol-logo.png";
+
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+
+import badgeLogo from "../../../assets/sindpol-logo.png";
+import penalLogo from "../../../assets/penal_df-min.png.png";
+import qrCode from "../../../assets/qr-code.png";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Carteirinha = () => {
-    const { user } = useAuth();
-    const permissions = usePermissions();
-    const list = {titular:"Dannyeclisson", dataDeNascimento:"24/11/2001", dataExpedicao:"21/06/2019", CPF:"074.885.581-54", validade:"teste"}
+  const cardRef = useRef(null);
+
+  const downloadPDF = async () => {
+    const element = cardRef.current;
+    const scale = 3; 
+    const canvas = await html2canvas(element, {
+      scale,
+      useCORS: true, 
+    });
+    const imgData = canvas.toDataURL("image/png");
+  
+    // Criação do PDF
+    const pdf = new jsPDF("p", "mm", "a4");
+  
+    
+    const cardWidth = 100; 
+    const cardHeight = (canvas.height * cardWidth) / canvas.width; 
+    const xOffset = (pdf.internal.pageSize.getWidth() - cardWidth) / 2; 
+    const yOffset = 50; 
+  
+    pdf.addImage(imgData, "PNG", xOffset, yOffset, cardWidth, cardHeight);
+    pdf.save("carteirinha.pdf");
+  };
+  
+    const list = {
+        titular: "Dannyeclisson",
+        dataDeNascimento: "24/11/2001",
+        dataExpedicao: "21/06/2019",
+        CPF: "074.885.581-54",
+        validade: "30/11/2024",
+    };
+
     return (
-        user && (
-            <section className="container">
-                <div className="card">
-                    <header className="carteirinha-header">
-                        <h1>SINDPOL-DF</h1>
-                        <p>SINDICATO DOS POLICIAIS PENAIS DO DISTRITO FEDERAL</p>
-                    </header>
+        <div className="carteirinha-container">
+            {/* Primeira Parte da Carteirinha */}
+            <div className="carteirinha">
+                <header className="carteirinha-header">
+                    <h1>SINDPOL-DF</h1>
+                    <p>SINDICATO DOS POLICIAIS PENAIS DO DISTRITO FEDERAL</p>
+                </header>
+
+                {/* Informações e Badge */}
+                <div className="info-and-badge">
                     <div className="carteirinha-info">
-                        <body className="titular">
-                        <strong>TITULAR: </strong>
-                        <div>{list.titular}</div>
-                        </body>
-                        <div className="datas">
-                        <div className="dataDeNascimento">
-                        <p > <strong>DATA DE NASCIMENTO:</strong> </p> <br/> <p><spam className="teste"> {list.dataDeNascimento}</spam> </p>
+                        <div className="info-line">
+                            <div className="info-block">
+                                <strong>TITULAR:</strong><br/>
+                                <p className="info-color"><span>{list.titular}</span></p>
+                            </div>
                         </div>
-                        <div className="dataExpedicao">
-                        <p > <strong>DATA DE EXPEDIÇÃO:</strong> </p> <br/> <p><spam className="teste"> {list.dataExpedicao}</spam> </p>
+                        <div className="info-line">
+                            <div className="info-block">
+                                <strong>DATA DE NASCIMENTO:</strong><br/>
+                                <p className="info-color"><span>{list.dataDeNascimento}</span></p>
+                            </div>
+                            <div className="info-block">
+                                <strong>DATA DE EXPEDIÇÃO:</strong><br/>
+                                <p className="info-color"><span>{list.dataExpedicao}</span></p>
+                            </div>
                         </div>
+                        <div className="info-line">
+                            <div className="info-block">
+                                <strong>CPF:</strong><br/>
+                                <p className="info-color"><span>{list.CPF}</span></p>
+                            </div>
+                            <div className="info-block">
+                                <strong>VALIDADE:</strong><br/>
+                                <p className="info-color"><span>{list.validade}</span></p>
+                            </div>
                         </div>
-                        <div className="CPFValidade">
-                        <div className="CPF">
-                        <p><strong>CPF:</strong></p> <br/> <p> <spam className="teste"> {list.CPF}</spam> </p>
-                        </div>
-                        <div className="validade">
-                        <p><strong>VALIDADE:</strong></p> <br/> <p> <spam className="teste"> {list.validade}</spam> </p>
-                        </div>
-                        </div>
-                        <body>
-                        <img src="LogoSentinela.png"/>
-                        </body>
+                    </div>
+
+                    {/* Badge Section */}
+                    <div className="badge-section">
+                        <img src={badgeLogo} alt="Sindicalizado Badge" className="badge-logo" />
+                        <p className="Sind">SINDICALIZADO</p>
                     </div>
                 </div>
+            </div>
 
+            {/* Segunda Parte da Carteirinha */}
+            <div className="carteirinha">
+            <footer className="carteirinha-footer">
+                    <p>
+                        Setor de Diversões Sul (SDS), Conjunto Baracat, Bloco F, 27, Salas 313/315,
+                        Asa Sul, Brasília/DF, CEP 70392-900
+                    </p>
+                    </footer>
+                <div className="qr-section">
+                    <img src={qrCode} alt="QR Code" className="qr-code" />
+                    <p className="qr-code-numero">0000</p>
+                </div>
 
-            </section>
-        )
+                <footer className="carteirinha-footer">
+                    <p>sindpol.org.br / contato@sindpol.org.br</p>
+                    <div className="social-media">
+                        <FaInstagram className="social-icon" />
+                        <FaYoutube className="social-icon" />
+                        <FaFacebook className="social-icon" />
+                        <FaTwitter className="social-icon" />
+                    </div>
+                    <span>@sindpoldf</span>
+                    <div className="footer-logos">
+                    <img src={badgeLogo} alt="Logo 1" className="footer-logo" />
+                    <img src={penalLogo} alt="Logo 2" className="footer-logo" />
+                    </div>
+                </footer>
+            </div>
+
+            {/* Botão */}
+            <button onClick={downloadPDF} className="download-button">BAIXAR CARTEIRINHA</button>
+        </div>
     );
 };
 
