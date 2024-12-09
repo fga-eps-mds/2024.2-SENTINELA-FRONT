666b4a6c802e2ae9b3a7e6c7ce768b3ba9256ffa
RodrigoAmaral <rodrigoamaral1705@gmail.com>
2024-12-09 02:23:34 -0300

    Correção problema build

--- src/Routes/publicRoutes.jsx
+++ src/Routes/publicRoutes.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import MemberShip from "../Pages/Public/MemberShip";
 import PasswordRecovery from "../Pages/Public/PasswordRecovery";
 import ChangePasswordPage from "../Pages/Public/ChangePasswordPage";
-import Advantages from "../Pages/Public/Advantages";
 import LoginNovo from "../Pages/Public/LoginNovo";
 import VerifyMemberForm from "../Pages/Public/VerifyMember/VerifyMemberForm";
 import VerifyMemberActiveStatus from "../Pages/Public/VerifyMember/VerifyMemberActiveStatus";
@@ -13,7 +12,6 @@ const PublicRoutes = () => {
     <Routes>
       <Route path="/" element={<LoginNovo />} />
       <Route path="/filiacao" element={<MemberShip />} />
-      <Route path="/vantagens" element={<Advantages />} />
       <Route path="/recuperar-senha" element={<PasswordRecovery />} />
       <Route path="/trocar-senha/:token" element={<ChangePasswordPage />} />
       <Route path="/verificar-membro" element={<VerifyMemberForm />} />
