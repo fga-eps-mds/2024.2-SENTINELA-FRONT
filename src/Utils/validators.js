3a5ba84e2b02839a1b85901731d21cda1cf26a09
redjsun <yzabellamiranda4@gmail.com>
2024-12-13 22:51:27 -0300

    validacao de cpf

--- src/Utils/validators.js
+++ src/Utils/validators.js
@@ -81,6 +81,48 @@ export const mascaraCEP = (cep) => {
   return formattedCEP.replace(/(\d{5})(\d)/, "$1-$2");
 };
 
+export const validarCPF = (cpf) => {
+  cpf = cpf.replace(/[^\d]+/g, "");
+
+  if (cpf.length !== 11) {
+      console.error("Erro: CPF deve conter 11 d�gitos");
+      return false;
+  }
+
+  if (/^(\d)\1+$/.test(cpf)) {
+      console.error("Erro: CPF n�o pode conter todos d�gitos iguais");
+      return false;
+  }
+
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+      soma += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) {
+      resto = 0;
+  }
+  if (resto !== parseInt(cpf.charAt(9))) {
+      console.error("Erro: Primeiro d�gito verificador do CPF � inv�lido");
+      return false;
+  }
+
+  soma = 0;
+  for (let i = 0; i < 10; i++) {
+      soma += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) {
+      resto = 0;
+  }
+  if (resto !== parseInt(cpf.charAt(10))) {
+      console.error("Erro: Segundo d�gito verificador do CPF � inv�lido");
+      return false;
+  }
+
+  return true;
+};
+
 export const handleCpfCnpjInput = (value) => {
   const numericValue = value.replace(/\D/g, "");
   if (numericValue.length <= 11) {
