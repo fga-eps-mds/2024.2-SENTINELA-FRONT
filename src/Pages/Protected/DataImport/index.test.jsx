924e174fe761c5da7eb58a9ab6910a5c62be3fc7
Igor <igor.veras.daniel@gmail.com>
2025-01-26 12:07:22 -0300

    Início dos testes

---
+++ src/Pages/Protected/DataImport/index.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { vi } from "vitest";
+import { APIBank } from "../../../Services/BaseService";
+import {
+  createFinancialMovements,
+  updateFinancialMovementsById,
+} from "../../../Services/FinancialMovementsService";
+import DataImport from "./index";
+import "@testing-library/jest-dom";
+
+vi.mock("../../../Services/BaseService", () => ({
+  APIBank: {
+    get: vi.fn(),
+  },
+}));
+
+vi.mock("../../../Services/FinancialMovementsService", () => ({
+  createFinancialMovements: vi.fn(),
+  updateFinancialMovementsById: vi.fn(),
+}));
+
+describe("DataImport Component", () => {
+  beforeEach(() => {
+    localStorage.setItem("@App:user", JSON.stringify({ token: "mock-token" }));
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks(); // Limpa todos os mocks após cada teste
+    localStorage.clear(); // Limpa o localStorage após cada teste
+  });
+
+  it("renders the component correctly", () => {
+    render(<DataImport />);
+
+    // Verifica se os elementos estão sendo renderizados corretamente
+    expect(screen.getByText("Importar Extrato Bancário")).toBeInTheDocument();
+    expect(screen.getByText("SELECIONE UM ARQUIVO")).toBeInTheDocument();
+    expect(screen.getByText("SALVAR")).toBeInTheDocument();
+  });
+
+});
