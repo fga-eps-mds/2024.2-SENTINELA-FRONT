78615ea806cf5edd77df2a8e24f776d766cc55a2
daniso0412 <180015222@aluno.unb.br>
2025-02-04 11:54:57 -0300

    teste usePermissions.test.jsx corrigido

--- src/Utils/usePermissions.test.jsx
+++ src/Utils/usePermissions.test.jsx
@@ -1,22 +1,23 @@
-import React from "react";
+import { describe, expect, beforeEach, vi } from "vitest";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import PermissionCRUD from "./../Pages/Protected/Permissions/permissionsHandler";
 import * as BaseService from "./../Services/BaseService/index";
 import * as LoaderFunctions from "./../Services/Functions/loader";
 import * as PermissionUtils from "../Utils/permission";
+import "@testing-library/jest-dom";
 
-BaseService.APIUsers = {
-  get: jest.fn(),
-  post: jest.fn(),
-  patch: jest.fn(),
-  delete: jest.fn(),
-};
-LoaderFunctions.getToken = jest.fn();
-PermissionUtils.checkAction = jest.fn();
+
+vi.spyOn(BaseService.APIUsers, "get").mockImplementation(() => Promise.resolve({ data: [] }));
+vi.spyOn(BaseService.APIUsers, "post").mockImplementation(() => Promise.resolve());
+vi.spyOn(BaseService.APIUsers, "patch").mockImplementation(() => Promise.resolve());
+vi.spyOn(BaseService.APIUsers, "delete").mockImplementation(() => Promise.resolve());
+
+vi.spyOn(LoaderFunctions, "getToken").mockReturnValue("mocked_token");
+vi.spyOn(PermissionUtils, "checkAction").mockReturnValue(true);
 
 describe("PermissionCRUD Component", () => {
   beforeEach(() => {
-    jest.clearAllMocks(); // Limpa os mocks antes de cada teste
+    vi.clearAllMocks(); // Limpa os mocks antes de cada teste
   });
 
   test('deve renderizar o título corretamente', () => {
@@ -24,16 +25,13 @@ describe("PermissionCRUD Component", () => {
     const title = screen.getByText(/Permission Management/i);
     expect(title).toBeInTheDocument();
   });
-  
 
   test("deve chamar fetchPermissions ao carregar o componente", async () => {
-    BaseService.APIUsers.get.mockResolvedValueOnce({ data: [] });
     render(<PermissionCRUD />);
     await waitFor(() => expect(BaseService.APIUsers.get).toHaveBeenCalledTimes(1));
   });
 
   test("deve permitir a criação de permissões se o usuário tiver permissão", async () => {
-    PermissionUtils.checkAction.mockReturnValueOnce(true); // Simula permissão
     render(<PermissionCRUD />);
     const input = screen.getByLabelText(/Permission Name/i);
     const button = screen.getByText(/Criar Permissão/i);
@@ -52,10 +50,10 @@ describe("PermissionCRUD Component", () => {
   test("deve exibir permissões na tabela", async () => {
     const mockPermissions = [{ _id: "1", name: "Admin" }];
     BaseService.APIUsers.get.mockResolvedValueOnce({ data: mockPermissions });
+
     render(<PermissionCRUD />);
     await waitFor(() => {
-      const permissionName = screen.getByText(/Admin/i);
-      expect(permissionName).toBeInTheDocument();
+      expect(screen.getByText(/Admin/i)).toBeInTheDocument();
     });
   });
 });
